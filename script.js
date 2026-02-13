86cbe4bb20cda3787195bec371a793cb5ad29d75
vish863 <vaishakhikaravadra669@gmail.com>
2026-02-13 23:26:54 +0530

    Valantine 1

---
+++ script.js
@@ -0,0 +1,13 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+
+yesBtn.addEventListener("click", () => {
+  message.innerText = "YAYYY 😍💖 I knew it!";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
